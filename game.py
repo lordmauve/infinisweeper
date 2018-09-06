00e6a5a48a054ad421216eb2e29c4c897de088b0
Daniel Pope <mauve@mauveweb.co.uk>
2018-09-06 20:23:10 +0100

    Change state of tiles

--- game.py
+++ game.py
@@ -1,3 +1,5 @@
+from enum import Enum
+import random
 TILE = 32
 
 TILESW = 20
@@ -9,8 +11,57 @@ HEIGHT = TILESH * TILE
 
 viewport_y = 0
 
+
+class TileTypes(Enum):
+    BLANK = 'blank'
+    N1 = 'one'
+    N2 = 'two'
+    N3 = 'three'
+    N4 = 'four'
+    N5 = 'five'
+    N6 = 'six'
+    N7 = 'seven'
+    N8 = 'eight'
+    MINE = 'mine'
+
+
+types = list(TileTypes)
+
+
+class State(Enum):
+    COVERED = 1
+    UNCOVERED = 2
+    FLAGGED = 3
+
+
+
+class Tile:
+    def __init__(self, state=None):
+        self.flagged = False
+        self.state = state or State.COVERED
+        self.type = random.choice(types)
+
+    @property
+    def image(self):
+        if self.state is State.COVERED:
+            return 'cover'
+        elif self.state is State.FLAGGED:
+            return 'flag'
+        else:
+            return self.type.value
+
+    def on_uncover(self):
+        self.state = State.UNCOVERED
+
+    def on_flag(self):
+        if self.state is State.COVERED:
+            self.state = State.FLAGGED
+        elif self.state is State.FLAGGED:
+            self.state = State.COVERED
+
+
 LINES = [
-    ['cover'] * TILESW
+    [Tile() for _ in range(TILESW)]
     for _ in range(1000)
 ]
 
@@ -18,8 +69,8 @@ LINES = [
 def draw():
     screen.fill('red')
     for y, row in enumerate(LINES):
-        for x, val in enumerate(row):
-            screen.blit(val, (x * TILE, HEIGHT - (y + 1) * TILE + viewport_y))
+        for x, tile in enumerate(row):
+            screen.blit(tile.image, (x * TILE, HEIGHT - (y + 1) * TILE + viewport_y))
 
 
 def update(dt):
@@ -32,5 +83,9 @@ def on_mouse_down(pos, button):
     tx = x // TILE
     ty = int(HEIGHT - y + viewport_y) // TILE
     print(f"Button {button} pressed at {(tx, ty)}")
-    LINES[ty][tx] = 'blank'
+    current = LINES[ty][tx]
+    if button == mouse.LEFT:
+        current.on_uncover()
+    elif button == mouse.RIGHT:
+        current.on_flag()
 
