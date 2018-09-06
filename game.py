2fae91dab49c6f6831b690400c92083861fd50ef
Daniel Pope <mauve@mauveweb.co.uk>
2018-09-06 20:36:52 +0100

    Game over condition

--- game.py
+++ game.py
@@ -8,6 +8,7 @@ TILESH = 16
 WIDTH = TILESW * TILE
 HEIGHT = TILESH * TILE
 
+WINNING = True
 
 viewport_y = 0
 
@@ -59,23 +60,73 @@ class Tile:
         elif self.state is State.FLAGGED:
             self.state = State.COVERED
 
+    def is_dangerous(self):
+        if (self.state is State.COVERED and self.type is TileTypes.MINE) or \
+           (self.state is State.FLAGGED and self.type is not TileTypes.MINE):
+            return True
+        return False
 
-LINES = [
-    [Tile() for _ in range(TILESW)]
-    for _ in range(1000)
-]
+
+
+LINES = None
+
+
+def generate_grid():
+    global LINES
+    LINES = [
+        [Tile() for _ in range(TILESW)]
+        for _ in range(1000)
+    ]
+
+generate_grid()
 
 
 def draw():
     screen.fill('red')
     for y, row in enumerate(LINES):
         for x, tile in enumerate(row):
-            screen.blit(tile.image, (x * TILE, HEIGHT - (y + 1) * TILE + viewport_y))
+            img = tile.image
+            if not WINNING:
+                img = tile.type.value
+            screen.blit(img, (x * TILE, HEIGHT - (y + 1) * TILE + viewport_y))
+
+    if not WINNING:
+        screen.draw.text(
+            'Game Over',
+            color=(180, 0, 0),
+            fontsize=100,
+            center=(WIDTH // 2, HEIGHT // 2)
+        )
+
+
+last_check_row = 0
 
 
 def update(dt):
-    global viewport_y
+    global viewport_y, last_check_row, WINNING
     viewport_y += TILE * 0.25 * dt
+    check_row = int(viewport_y // TILE)
+    if check_row > last_check_row and WINNING:
+        last_check_row = check_row
+        for x in range(TILESW):
+            t = LINES[check_row][x]
+            if t.is_dangerous():
+                game_over()
+                break
+
+
+
+def game_over():
+    global WINNING
+    WINNING = False
+    clock.schedule_unique(restart, 4)
+
+
+def restart():
+    global WINNING, viewport_y
+    WINNING = True
+    viewport_y = 0
+    generate_grid()
 
 
 def on_mouse_down(pos, button):
