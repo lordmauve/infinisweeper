a536bff9da0d1ab569b1670f39328a90929b5a42
Daniel Pope <mauve@mauveweb.co.uk>
2018-09-06 19:54:10 +0100

    Draw minesweeper grid

--- game.py
+++ game.py
@@ -1,6 +1,30 @@
+TILE = 32
+
+TILESW = 20
+TILESH = 16
+
+WIDTH = TILESW * TILE
+HEIGHT = TILESH * TILE
+
+
+y = 0
+
+
 def draw():
     screen.fill('red')
+    for x in range(TILESW):
+        for y in range(TILESH):
+            screen.blit('blank', (x * TILE, y * TILE))
+
+
+def update(dt):
+    global y
+    y += TILE * 0.25 * dt
+
 
 def on_mouse_down(pos, button):
-    print(f"Button {button} pressed at {pos}")
 
+    x, y = pos
+    tx = x // TILE
+    ty = y // TILE
+    print(f"Button {button} pressed at {(tx, ty)}")
