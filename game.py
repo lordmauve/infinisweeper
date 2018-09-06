f974833c532027a11e676ef5c0397fc8e806cfc3
Daniel Pope <mauve@mauveweb.co.uk>
2018-09-06 20:08:49 +0100

    Scrolling grid

--- game.py
+++ game.py
@@ -7,24 +7,30 @@ WIDTH = TILESW * TILE
 HEIGHT = TILESH * TILE
 
 
-y = 0
+viewport_y = 0
+
+LINES = [
+    ['cover'] * TILESW
+    for _ in range(1000)
+]
 
 
 def draw():
     screen.fill('red')
-    for x in range(TILESW):
-        for y in range(TILESH):
-            screen.blit('blank', (x * TILE, y * TILE))
+    for y, row in enumerate(LINES):
+        for x, val in enumerate(row):
+            screen.blit(val, (x * TILE, HEIGHT - (y + 1) * TILE + viewport_y))
 
 
 def update(dt):
-    global y
-    y += TILE * 0.25 * dt
+    global viewport_y
+    viewport_y += TILE * 0.25 * dt
 
 
 def on_mouse_down(pos, button):
-
     x, y = pos
     tx = x // TILE
-    ty = y // TILE
+    ty = int(HEIGHT - y + viewport_y) // TILE
     print(f"Button {button} pressed at {(tx, ty)}")
+    LINES[ty][tx] = 'blank'
+
